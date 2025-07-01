f493c485e6e0a0df389a3b4daac76606782b7e9b
AaravB2121 <47925580+AaravB2121@users.noreply.github.com>
2025-06-30 22:10:33 -0500

    worked on the player stats

---
+++ scenes/health.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+
+@export var MAX_HEALTH := 10.0
+var health : flat
+
+func _ready():
+	health = MAX_HEALTH
+	
+func damage (attack: Attack)
+	health -= attack.attack_damage
+	
+	if health <= 0:
+		get_parent().queue_free()
